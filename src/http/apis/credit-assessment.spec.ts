e58aa9e96c43a0c34f59ed92d32ba1dcc46141d3
Oscar Blomqvist <oscar@ourstudio.se>
2021-01-22 11:05:29 +0100

    Add test for new case request error

---
+++ src/http/apis/credit-assessment.spec.ts
@@ -0,0 +1,38 @@
+const fixtures = require("../../../test/fixtures");
+const fixture = (name: string, withValues: any = undefined): any =>
+    fixtures.create(name, withValues);
+
+const http = require("..");
+
+import { newCase } from "./credit-assessment";
+
+describe("API: Credit Assessment", () => {
+    describe("Given unsuccessful request", () => {
+        beforeAll(() => {
+            http.json = jest.fn(
+                () =>
+                    new Promise((resolve) => {
+                        const response = fixture("IApiResponse", {
+                            successful: false,
+                        });
+                        resolve(response);
+                    })
+            );
+        });
+
+        describe("newCase()", () => {
+            it("Throws error", async () => {
+                const inquiry = fixture("ICreditAssessmentInquiry");
+
+                let err: any;
+                try {
+                    await newCase(inquiry);
+                } catch (e) {
+                    err = e;
+                }
+
+                expect(err).toBeInstanceOf(Error);
+            });
+        });
+    });
+});
